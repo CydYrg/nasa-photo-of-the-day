47985b4843dfc688a665439cd863f96cd1ac7f4d
CydYrg <ceyda_yurga@hotmail.com>
2021-05-12 23:17:16 +0100

    AstronomyCard and AtronomyContainer.js added also worked on code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import AstronomyContainer from './components/AstronomyContainer';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <AstronomyContainer />
+    /*<div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
     </div>
   );
-}
+}*/
 
 export default App;
