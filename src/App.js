02c103c2e6d29ebf31bfe973b25a762ae3352daf
CydYrg <ceyda_yurga@hotmail.com>
2021-05-14 00:20:43 +0100

    added some code on app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import AstronomyContainer from './AstronomyContainer';
 import "./App.css";
+import axios from "axios";
+const apiKey = "keJGfKsMKsmMZPDSLpdvoEfw8Zs7AmIF7nhilnd3";
 
 function App() {
+  const [nasaData, setNasaData] = useState([])
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`)
+      .then((res) => {
+        setNasaData([...nasaData, ...res.data])
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    }, [count])
   return (
-    <AstronomyContainer />
+    <div  className="App">
+
+    </div>
     /*<div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
