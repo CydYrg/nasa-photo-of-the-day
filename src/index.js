81891c7e2c16d7c42255b9e2526de2466be31f76
Ceyda Yurga <ceydayurga@Ceydas-Air.lan>
2021-06-10 00:02:03 +0100

    worked on all files

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import './assets/stylesheets/style.scss';
+
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod?keJGfKsMKsmMZPDSLpdvoEfw8Zs7AmIF7nhilnd3'
+export const API_KEY = 'keJGfKsMKsmMZPDSLpdvoEfw8Zs7AmIF7nhilnd3'
 
 ReactDOM.render(<App />, document.getElementById("root"));
